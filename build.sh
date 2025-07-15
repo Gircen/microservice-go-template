a0812346257e6a8d7c48f9f7fa0c88ac76ed68e5
antonkolesnikov <girrcen666@icloud.com>
2025-07-15 17:56:32 +0300

    add run.sh and update build.s

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-export CONFIG_MAP_GO="./kubernate/configmap.yml"
-go build -o ./build src/main/*
+GOROOT=/Users/antonkolesnikov/go/go1.23rc1 #gosetup
+GOPATH=/Users/antonkolesnikov/go #gosetup
+go build -o build/microservice-go-template microservice-go-template/src/main #gosetup
+build/microservice-go-template
---
+++ run.sh
@@ -0,0 +1,4 @@
+GOROOT={GO_PATH}/go/go1.23rc1 #gosetup
+GOPATH={GO_PATH}/go #gosetup
+go build -o build/microservice-go-template microservice-go-template/src/main #gosetup
+build/microservice-go-template
