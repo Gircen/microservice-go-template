6eaa980c0dde4e4ae94d992f29e08df2e9831eb5
antonkolesnikov <girrcen666@icloud.com>
2025-07-29 11:59:49 +0300

    move config and healthService.go to core

--- src/main/main.go
+++ src/main/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/Gircen/go-library-core/main/config"
-	"microservice-go-template/src/main/service"
+	"github.com/Gircen/go-library-core/main/logs"
+	"github.com/Gircen/go-library-core/main/service"
+	"microservice-go-template/src/main/service/crud"
 )
 
 func main() {
+
 	var cfg = config.GetConfig()
-	service.Run(cfg)
+	service.RunServiceCore(cfg)
+	logs.Logger.Println("ERRRRR")
+	crud.Run(cfg)
 }
